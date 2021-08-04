d2d7863b22ec08a56d9260f2a42eb9c8bdcc62ab
Harshit Saini <harshit.saini.harshit@gmail.com>
2021-08-04 14:22:26 +0530

    fix websocket error

--- blackjack/asgi.py
+++ blackjack/asgi.py
@@ -9,22 +9,21 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blackjack.settings')
-django.setup()
-
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from game.consumers import GameRoom
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blackjack.settings')
+django_asgi_app = get_asgi_application()
+django.setup()
 
 ws_patterns = [
     path("ws/game/<room_code>", GameRoom.as_asgi())
 ]
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(URLRouter(ws_patterns))
 })
