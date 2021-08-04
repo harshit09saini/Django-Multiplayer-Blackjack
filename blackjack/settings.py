5fbb18d66cef2d0d4e487c2dd9b347c48bae699a
Harshit Saini <harshit.saini.harshit@gmail.com>
2021-08-04 12:04:05 +0530

    config for deployment

--- blackjack/settings.py
+++ blackjack/settings.py
@@ -21,15 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    # "192.168.1.9",
-    # "127.0.0.1",
-    # "0.0.0.0"
-]
+ALLOWED_HOSTS = ['0.0.0.0',
+                 'localhost',
+                 '127.0.0.1',
+                 'blackjack-django.herokuapp.com']
 
 # Application definition
 
